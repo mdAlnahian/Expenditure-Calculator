aed8475237ff9603a77d6c6bdf937f9d541ea893
mdAlnahian <muhammedalnahian@gmail.com>
2022-02-17 12:17:34 +0600

    gkjgsdhgf

--- js/index.js
+++ js/index.js
@@ -22,29 +22,39 @@ document
 //total expenses calculating************//
 function totalExpenses() {
   let primaryExpenses = document.getElementById("total-expenses");
-  let primaryExpensesText = primaryExpenses.innerText;
-  let primaryExpensesTextFloat = parseFloat(primaryExpensesText);
-  primaryExpenses.innerText =
-    inputDataCollection("second") +
-    inputDataCollection("third") +
-    inputDataCollection("fourth");
+  // let primaryExpensesText = primaryExpenses.innerText;
+  // let primaryExpensesTextFloat = parseFloat(primaryExpensesText);
+  // primaryExpenses.innerText =
+    // inputDataCollection("second") +
+    // inputDataCollection("third") +
+    // inputDataCollection("fourth");
+    let primaryExpensesValue =
+      inputDataCollection("second") +
+      inputDataCollection("third") +
+      inputDataCollection("fourth");
   // using-condition
-  if (primaryExpenses.innerText <= inputDataCollection("first")) {
-    // primaryExpenses.innerText = primaryExpenses.innerText;
-    return primaryExpenses.innerText;
-  } else {
-    let error1 = document.getElementById("error1");
-    error1.style.display = "block";
-  }
+ // if (primaryExpenses.innerText <= inputDataCollection("first"))
+  
+    if (primaryExpensesValue <= inputDataCollection("first")) {
+      // primaryExpenses.innerText = primaryExpenses.innerText;
+      // return primaryExpenses.innerText;
+      return primaryExpensesValue;
+    } else {
+      let error1 = document.getElementById("error1");
+      error1.style.display = "block";
+    }
 }
 //initial balance after giving the value of income
 function initialBalance() {
   let initialBalanceAmount = document.getElementById("balance");
-  let initialBalanceAmountData = initialBalanceAmount.innerText;
-  let initialBalanceAmountDataFloat = parseFloat(initialBalanceAmountData);
-  initialBalanceAmount.innerText =
+  // let initialBalanceAmountData = initialBalanceAmount.innerText;
+  // let initialBalanceAmountDataFloat = parseFloat(initialBalanceAmountData);
+  // initialBalanceAmount.innerText =
+  //   inputDataCollection("first") - totalExpenses();
+  initialBalanceAmountInnerText =
     inputDataCollection("first") - totalExpenses();
-  return initialBalanceAmount.innerText;
+  return initialBalanceAmountInnerText;
+  // return initialBalanceAmount.innerText;
 }
 
 // save button click handeling
@@ -66,11 +76,12 @@ function savingPercentageAmount() {
 // save amount function with error
 function saveYourMoney() {
   let moneyInput = document.getElementById("money-saved");
-  let moneyInputData = moneyInput.innerText;
-  let moneyInputDataFloat = parseFloat(moneyInputData);
+  // let moneyInputData = moneyInput.innerText;
+  // let moneyInputDataFloat = parseFloat(moneyInputData);
   let savingMoney =
     (savingPercentageAmount() * inputDataCollection("first")) / 100;
-  moneyInput.innerText = savingMoney;
+  // moneyInput.innerText = savingMoney;
+  // return savingMoney;
 
   // return moneyInput.innerText;
   //error2 msg
@@ -78,14 +89,15 @@ function saveYourMoney() {
     let error1 = document.getElementById("error2");
     error1.style.display = "block";
   } else {
-    return moneyInput.innerText;
+     return savingMoney;
   }
 }
 //remaining money after savings
 function remainingMoneyAfterSavings() {
   let remainingMoney = document.getElementById("remaining-balance");
-  let remainingMoneyData = remainingMoney.innerText;
-  let remainingMoneyDataFloat = parseFloat(remainingMoneyData);
-  remainingMoney.innerText = initialBalance() - saveYourMoney();
-  return remainingMoney.innerText;
+  // let remainingMoneyData = remainingMoney.innerText;
+  // let remainingMoneyDataFloat = parseFloat(remainingMoneyData);
+  // remainingMoney.innerText = initialBalance() - saveYourMoney();
+  let remainingMoneyData = initialBalance() - saveYourMoney();
+  return remainingMoneyData;
 }
